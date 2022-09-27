9b5fc122397307b248ff500ffd4d55da782cdb61
Andrei Hirleata <hirleata.andrei@gmail.com>
2022-09-27 17:12:41 +0100

    created list/generateors for trainee list

--- src/main/java/com/sparta/Trainee.java
+++ src/main/java/com/sparta/Trainee.java
@@ -1,8 +1,17 @@
 package com.sparta;
 
+import com.sparta.models.util.Randomizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trainee {
 	private static int idIncrement = 0;
 	private final int id;
+
+
+
+	private static List<Trainee> traineeList = new ArrayList<>();
 
 	public Trainee () {
 		this.id = idIncrement++;
@@ -10,5 +19,16 @@ public class Trainee {
 
 	public int getId () {
 		return id;
+	}
+
+	public static List<Trainee> getTraineeList() {
+		return traineeList;
+	}
+
+	public static void generateTrainees() {
+		int numOfTrainees = Randomizer.getRandom(50,100);
+		for(int i = 0; i <= numOfTrainees; i++) {
+			traineeList.add(new Trainee());
+		}
 	}
 }
