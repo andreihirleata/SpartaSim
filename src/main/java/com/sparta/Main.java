bdf931d8734edc879648f3ffe38a244b1d5b2b04
Daniel Booth <unconfigured@null.spigotmc.org>
2022-09-27 23:32:47 +0100

    - prints number of open centres - prints number of full centres - prints number of trainees currently training - print number of trainees on the waiting list opens doors for trainees to going a center (0-50)

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -1,23 +1,38 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.sparta;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner employeesInput = new Scanner(System.in);
-        System.out.println("Enter the number of trainees");
-         int trainees = employeesInput.nextInt();
-
-
-        Scanner inputMonths = new Scanner(System.in);
-        System.out.println("Enter the number of months");
-        int numberofMonths = inputMonths.nextInt();
-
+    public Main() {
     }
 
-    // add scanner for employes
-    // how many months
+    public static void main(String[] args) {
+        Academy academy = new Academy();
+        TrainingCenter center1 = new TrainingCenter();
+        TrainingCenter center2 = new TrainingCenter();
+        TrainingCenter center3 = new TrainingCenter();
+        academy.addCenter(center1);
 
-
+        //Scanner employeesInput = new Scanner(System.in);
+        //System.out.println("Enter the number of trainees");
+        //int trainees = employeesInput.nextInt();
+        //Scanner inputMonths = new Scanner(System.in);
+        //System.out.println("Enter the number of months");
+        //int numberofMonths = inputMonths.nextInt();
+        Trainee.generateTrainees();
+        Trainee.generateTrainees();
+        Trainee.openDoors();
+        Trainee.openDoors();
+        Trainee.openDoors();
+        Trainee.openDoors();
+        Trainee.getTraineeList().stream().forEach((t) -> {
+            System.out.println(t.getId());
+        });
+        System.out.println(academy);
+    }
 }
