09a73e869fb27c8df464603fe1248770b2872e17
Daniel Booth <unconfigured@null.spigotmc.org>
2022-09-28 11:26:52 +0100

    adds a center every two month and generates trainees every month

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -5,6 +5,8 @@
 
 package com.sparta;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,24 +14,24 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Academy academy = new Academy();
-        TrainingCenter center1 = new TrainingCenter();
-        TrainingCenter center2 = new TrainingCenter();
-        TrainingCenter center3 = new TrainingCenter();
-        academy.addCenter(center1);
+        Scanner inputMonths = new Scanner(System.in);
+        System.out.println("Enter the number of months");
+        int numberofMonths = inputMonths.nextInt();
 
+        Academy academy = new Academy();
         //Scanner employeesInput = new Scanner(System.in);
         //System.out.println("Enter the number of trainees");
         //int trainees = employeesInput.nextInt();
-        //Scanner inputMonths = new Scanner(System.in);
-        //System.out.println("Enter the number of months");
-        //int numberofMonths = inputMonths.nextInt();
-        Trainee.generateTrainees();
-        Trainee.generateTrainees();
-        Trainee.openDoors();
-        Trainee.openDoors();
-        Trainee.openDoors();
-        Trainee.openDoors();
+        for(int i=0;i<=numberofMonths;i++)
+        {
+            if(i%2==0)
+            {
+                academy.addCenter(new TrainingCenter());
+            }
+            Trainee.generateTrainees();
+            Trainee.openDoors();
+        }
+
         Trainee.getTraineeList().stream().forEach((t) -> {
             System.out.println(t.getId());
         });
